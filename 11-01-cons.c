97eb66a168bf5f6e6e1b126320ca402415e8a4d1
fra-dln <kecco9939@gmail.com>
2022-03-01 06:05:56 -0500

    final ed

--- 11-01-cons.c
+++ 11-01-cons.c
@@ -10,3 +10,71 @@ affinchè abbia effettivamente completato la lettura.
 Il produttore prende il dato da stdin, mentre il
 consumatore stampa il dato a stdout.
 */
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <stdio.h>
+
+#define MEMSIZE 4096
+
+int cons(void *shared_address, int semaphore){
+    struct sembuf buf[1];
+
+	buf[0].sem_num = 0;
+	buf[0].sem_op = -1;
+	buf[0].sem_flg = 0;
+
+    if(semop(semaphore, buf, 1)==-1){
+        printf("CAn't sync semaphore\n");
+        return 0;
+    }
+
+    printf("Message from prod: %s\n", (char *) shared_address);
+
+    buf[0].sem_num = 1;
+	buf[0].sem_op = 1;
+	buf[0].sem_flg = 0;
+    
+    if(semop(semaphore, buf, 1)==-1){
+        printf("Can't sync semaphore \n");
+        return 0;
+    }
+    return 1;
+}
+
+int main(int argc, char **argv){
+    int ret, shm_ds,sem_ds;
+    key_t shm_key, sem_key;
+    void *shm_addr;
+
+    shm_key=6969;
+    sem_key=7878;
+    
+    shm_ds=shmget(shm_key, MEMSIZE,0);
+    if(shm_ds==-1){
+        printf("Can't get shared memory\n");
+        return -1;
+    }
+
+    shm_addr=shmat(shm_ds, NULL, 0);
+    if(shm_addr== (void *)-1){
+        printf("Can't link shared memory\n");
+        return -1;
+    }
+
+    sem_ds = semget(sem_key,2,0);
+    if (sem_ds==-1){
+        printf("Can't get semaphore \n%d",sem_ds);
+        shmdt(shm_addr);
+        return -1;
+    }
+
+    do{
+        ret=cons(shm_addr,sem_ds);
+    }while(ret);
+
+    shmdt(shm_addr);
+    return 0;
+}
--- 11-01-prod.c
+++ 11-01-prod.c
@@ -10,3 +10,110 @@ affinchè abbia effettivamente completato la lettura.
 Il produttore prende il dato da stdin, mentre il
 consumatore stampa il dato a stdout.
 */
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <stdio.h>
+
+#define MEMSIZE 4096
+
+union semun {
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+	struct seminfo *__buf;
+};
+
+int production(void *shared_address, int semaphore){
+    struct sembuf buf[1];
+
+    buf[0].sem_num=1;
+    buf[0].sem_op=-1;
+    buf[0].sem_flg=0;
+
+    if(semop(semaphore, buf, 1) == -1){
+        printf("Can't sync semaphore\n");
+        return 0;
+    }
+
+    printf("Message for the consumer: ");
+    if(scanf("%[^\n]", (char *) shared_address)==0){
+        *((char *)shared_address) = '\0';
+    }
+    getc(stdin);
+ 
+    buf[0].sem_num=0;
+    buf[0].sem_op=1;
+    buf[0].sem_flg=0;
+   
+    if(semop(semaphore,buf,1)==-1){
+        printf("Can't sync semaphore \n");
+        return 0;
+    }
+
+    return 1;
+
+    
+}
+
+int main(int argc, char **argv){
+    
+    key_t shm_key, sem_key;
+    int shm_ds, sem_ds;
+    void *shm_addr;
+
+    union semun sem_arg;
+
+    shm_key=6969;
+    sem_key=7878;
+
+    shm_ds=shmget(shm_key, MEMSIZE, IPC_CREAT|0666);
+    if(shm_ds==-1){
+        printf("Unable to allocate shared memory, exiting.\n");
+        return -1;
+    }
+
+    shm_addr=shmat(shm_ds,NULL,0);
+    if(shm_addr==(void *)-1){
+        printf("Unable to link the shared memory\n");
+        shmctl(shm_ds, IPC_RMID, NULL);
+        return -1;
+    }
+
+    sem_ds=semget(sem_key,2,IPC_CREAT|0666);
+    if(sem_ds==-1){
+        printf("Unable to get semaphore, exiting\n");
+        shmctl(shm_ds, IPC_RMID, NULL);
+        shmdt(shm_addr);
+        return -1;
+    }
+
+    sem_arg.val=0;
+    if(semctl(sem_ds, 0, SETVAL, sem_arg)==-1){
+        printf("Unable to init semaphore\n");
+        semctl(sem_ds,-1,IPC_RMID,sem_arg);
+        shmctl(shm_ds, IPC_RMID, NULL);
+        shmdt(shm_addr);
+        return -1;
+    }
+
+    sem_arg.val = 1;
+    if((semctl(sem_ds, 1, SETVAL, sem_arg))==-1){
+        printf("Unable to init semaphore\n");
+        semctl(sem_ds,-1,IPC_RMID,sem_arg);
+        shmctl(shm_ds, IPC_RMID, NULL);
+        shmdt(shm_addr);
+        return -1;
+    }
+    int ret;
+
+    do{
+        ret=production(shm_addr,sem_ds);
+    }while(ret);
+    
+    semctl(sem_ds , -1 , IPC_RMID , sem_arg);
+    shmctl(shm_ds, IPC_RMID, NULL);
+    shmdt(shm_addr);
+    return 0;
+}
